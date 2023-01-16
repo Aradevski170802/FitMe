8e14b433fb40e62dea108a048d274b5da29823c0
Nikos Karamousalidis <nkaramousalidis@york.citycollege.eu>
2023-01-16 19:47:16 +0200

    Created diets, bmi.Changed main.

--- other/bmiCalculator.js
+++ other/bmiCalculator.js
@@ -20,13 +20,13 @@ function calculateBMI() {
 		let bmi = (weight / ((height * height)
 							/ 10000)).toFixed(2);
 		if (bmi < 18.6) result.innerHTML =
-			`Under Weight : <span>${bmi}</span>`;
+			`Under Weight : ${bmi}`;
 
 		else if (bmi >= 18.6 && bmi < 24.9)
 			result.innerHTML =
-				`Normal : <span>${bmi}</span>`;
+				`Normal :  ${bmi}`;
 
 		else result.innerHTML =
-			`Over Weight : <span>${bmi}</span>`;
+			`Over Weight :  ${bmi}`;
 	}
 }
